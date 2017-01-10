dfe22a7e18b20e37e03f342074327e6dc8b71c57
Jose Annunziato <jannunzi@gmail.com>
2017-01-09 23:57:24 -0500

    migrating to heroku and mlab

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.PORT || 3000;
 
-app.listen(port, ipaddress);
+app.listen(port);
--- test/app.js
+++ test/app.js
@@ -6,12 +6,12 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
     }
 
     var mongoose = require("mongoose");
