83ed19b439aa7f69e56af6896a4f7ae1ddfe26fe
Jose Annunziato <jannunzi@gmail.com>
2017-05-17 15:07:14 -0400

    assignment 2 start

--- utilities/filelist.js
+++ utilities/filelist.js
@@ -6,11 +6,11 @@ app.use(function (req, res, next) {
         const secondToLastSlash = req.url.slice(0, -1).indexOf('/');
         const parent = folder.substring(0, secondToLastSlash);
         fs.readdir(folder, function (err, files) {
-            if(files.indexOf('index.html') > -1) {
-                next();
-            } else {
+            // if(files.indexOf('index.html') > -1) {
+            //     next();
+            // } else {
                 res.render('index', {files: files, folder: req.url, parent: parent});
-            }
+            // }
         });
     } else {
         next();
