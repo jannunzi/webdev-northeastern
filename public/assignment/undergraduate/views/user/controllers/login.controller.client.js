59f901dfd6de986af60be400edcfc104e27cfe6d
Jose Annunziato <jannunzi@gmail.com>
2017-05-24 13:27:46 -0400

    h3

--- public/assignment/undergraduate/views/user/controllers/login.controller.client.js
+++ public/assignment/undergraduate/views/user/controllers/login.controller.client.js
@@ -3,27 +3,19 @@
         .module('WAM')
         .controller('loginController', loginController);
     
-    function loginController($scope) {
+    function loginController($location, userService) {
 
-        var users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-        ];
+        var model = this;
 
-        $scope.login = function (username, password) {
-            var found = false;
-            for(var u in users) {
-                var user = users[u];
-                if(user.username === username && user.password === password) {
-                    found = true;
-                }
-            }
-            if(found) {
-                $scope.message = "Welcome " + username;
+        model.login = function (username, password) {
+
+            var found = userService.findUserByCredentials(username, password);
+            
+            if(found !== null) {
+                $location.url('/profile/' + found._id);
+                // $scope.message = "Welcome " + username;
             } else {
-                $scope.message = "Username " + username + " not found, please try again";
+                model.message = "Username " + username + " not found, please try again";
             }
         };
     }
