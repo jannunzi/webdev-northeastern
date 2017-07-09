6e842f56e7164cad6189e97e662cac3cfb5a8bd2
Jose Annunziato <jannunzi@gmail.com>
2017-07-09 19:51:49 -0400

    added filelist utility

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
+require("./utilities/filelist");
+
 app.use(express.static(__dirname + '/public'));
 
 require("./test/app");
