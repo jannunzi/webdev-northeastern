b6530aa37617d90ac464d2e093e1ea42fd54df88
Jose Annunziato <jannunzi@gmail.com>
2017-08-07 19:00:36 -0400

    6.5 create website for user id

--- assignment/models/user.schema.server.js
+++ assignment/models/user.schema.server.js
@@ -2,6 +2,8 @@ var mongoose = require("mongoose");
 var userSchema = mongoose.Schema({
     username: String,
     password: String,
+    roles: [{type: String, enum:["ADMIN", "STUDENT", "FACULTY"]}],
+    currentRole: String,
     firstName: String,
     lastName: String,
     websites: [{type: mongoose.Schema.Types.ObjectId, ref:"WebsiteModel"}],
--- assignment/models/website.model.server.js
+++ assignment/models/website.model.server.js
@@ -2,4 +2,11 @@ var mongoose = require("mongoose");
 var websiteSchema = require("./website.schema.server");
 var websiteModel = mongoose.model("WebsiteModel", websiteSchema);
 
+websiteModel.createWebsite = createWebsite;
+
 module.exports = websiteModel;
+
+function createWebsite(developerId, website) {
+    website.developer = developerId;
+    return websiteModel.create(website);
+}
